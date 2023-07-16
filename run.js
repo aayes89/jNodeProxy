f2c4ac9d0cae0752adb370dea2fb0d006446497d
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2023-07-16 13:02:53 -0600

    Update run.js
    more improvements

--- run.js
+++ run.js
@@ -14,27 +14,46 @@ app.get('/', (req, res) => {
     includeImages: req.query.includeImages || '',
     includeCss: req.query.includeCss || ''
   });
-  const proxyUrl = `/proxy?${query}`;
-  const fullProxyUrl = `${proxyUrl}&url=${encodeURIComponent(req.query.url || '')}`;
+  const proxyUrl = `/proxy?url=${encodeURIComponent(req.query.url || '')}&${query}`;
+
+  const includeJsChecked = req.query.includeJs ? 'checked' : '';
+  const includeImagesChecked = req.query.includeImages ? 'checked' : '';
+  const includeCssChecked = req.query.includeCss ? 'checked' : '';
 
   res.send(`
     <div style="background-color: #f2f2f2; padding: 10px;">
-      <h1>Proxy Web</h1>
-      <form method="get" action="${proxyUrl}">
+      <form id="proxyForm" method="get" action="/">
         <label for="url">URL:</label>
         <input type="text" id="url" name="url" value="${req.query.url || ''}">
         <label for="includeJs">Incluir JS:</label>
-        <input type="checkbox" id="includeJs" name="includeJs" ${req.query.includeJs ? 'checked' : ''}>
+        <input type="checkbox" id="includeJs" name="includeJs" ${includeJsChecked}>
         <label for="includeImages">Incluir Imágenes:</label>
-        <input type="checkbox" id="includeImages" name="includeImages" ${req.query.includeImages ? 'checked' : ''}>
+        <input type="checkbox" id="includeImages" name="includeImages" ${includeImagesChecked}>
         <label for="includeCss">Incluir CSS:</label>
-        <input type="checkbox" id="includeCss" name="includeCss" ${req.query.includeCss ? 'checked' : ''}>
+        <input type="checkbox" id="includeCss" name="includeCss" ${includeCssChecked}>
         <button type="submit">Cargar</button>
       </form>
     </div>
     <div style="margin-top: 20px;">
-      <iframe src="${fullProxyUrl}" frameborder="0" style="width: 100%; height: calc(100vh - 120px);"></iframe>
+      <iframe id="proxyFrame" src="${proxyUrl}" frameborder="0" style="width: 100%; height: calc(100vh - 120px);"></iframe>
     </div>
+    <script>
+      const proxyForm = document.getElementById('proxyForm');
+      const proxyFrame = document.getElementById('proxyFrame');
+
+      proxyForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const urlInput = document.getElementById('url');
+        const newUrl = encodeURIComponent(urlInput.value);
+        const query = new URLSearchParams({
+          includeJs: document.getElementById('includeJs').checked ? 'on' : '',
+          includeImages: document.getElementById('includeImages').checked ? 'on' : '',
+          includeCss: document.getElementById('includeCss').checked ? 'on' : ''
+        }).toString();
+        const newProxyUrl = '/proxy?url=' + newUrl + '&' + query;
+        proxyFrame.src = newProxyUrl;
+      });
+    </script>
   `);
 });
 
@@ -67,20 +86,49 @@ app.get('/proxy', (req, res) => {
       const href = $(element).attr('href');
       if (href && !url.parse(href).host) {
         const absoluteUrl = url.resolve(searchUrl, href);
-        const proxyHref = `/proxy?url=${encodeURIComponent(absoluteUrl)}&${query}`;
+        const proxyHref = '/proxy?url=' + encodeURIComponent(absoluteUrl) + '&' + query;
         $(element).attr('href', proxyHref);
       }
     });
 
-    $('img, script, link[rel="stylesheet"]').each((index, element) => {
-      const src = $(element).attr('src') || $(element).attr('href');
-      if (src && !url.parse(src).host) {
-        const absoluteUrl = url.resolve(searchUrl, src);
-        const proxySrc = `/proxy?url=${encodeURIComponent(absoluteUrl)}&${query}`;
-        $(element).attr('src', proxySrc);
-        $(element).attr('href', proxySrc);
-      }
-    });
+    if (req.query.includeImages) {
+      $('img').each((index, element) => {
+        const src = $(element).attr('src');
+        if (src && !url.parse(src).host) {
+          const absoluteUrl = url.resolve(searchUrl, src);
+          const proxySrc = '/proxy?url=' + encodeURIComponent(absoluteUrl) + '&' + query;
+          $(element).attr('src', proxySrc);
+        }
+      });
+    } else {
+      $('img').remove();
+    }
+
+    if (req.query.includeJs) {
+      $('script').each((index, element) => {
+        const src = $(element).attr('src');
+        if (src && !url.parse(src).host) {
+          const absoluteUrl = url.resolve(searchUrl, src);
+          const proxySrc = '/proxy?url=' + encodeURIComponent(absoluteUrl) + '&' + query;
+          $(element).attr('src', proxySrc);
+        }
+      });
+    } else {
+      $('script').remove();
+    }
+
+    if (req.query.includeCss) {
+      $('link[rel="stylesheet"]').each((index, element) => {
+        const href = $(element).attr('href');
+        if (href && !url.parse(href).host) {
+          const absoluteUrl = url.resolve(searchUrl, href);
+          const proxyHref = '/proxy?url=' + encodeURIComponent(absoluteUrl) + '&' + query;
+          $(element).attr('href', proxyHref);
+        }
+      });
+    } else {
+      $('link[rel="stylesheet"]').remove();
+    }
 
     res.send($.html());
   });
